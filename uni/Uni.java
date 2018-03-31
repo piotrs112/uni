3c231e712fe94cd734b8f611788a5e7e4fbf36f8
Falien164 <33525024+Falien164@users.noreply.github.com>
2018-03-31 17:15:02 +0200

    Add files via upload

--- uni/Uni.java
+++ uni/Uni.java
@@ -6,15 +6,20 @@
 package uni;
 /**
  *
- * @author Piotr Socha, Bartosz Sadowski, Łysik Mateusz
+ * @author Piotr Socha
+ * @author Bartosz Sadowski
+ * @author Łysik Mateusz
  */
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JLabel;
 import static javax.swing.JOptionPane.*;
 
 public class Uni extends JFrame implements ActionListener{
@@ -22,40 +27,49 @@ public class Uni extends JFrame implements ActionListener{
     private final int FRAME_WIDTH = 600;
     private final int FRAME_HEIGHT = 600; 
     
-    private int y=5, x=20;                            //do tworzenia przyciskow
     
-    private JButton bAddGroup = new JButton("Add group");
-    private JButton bRemoveGroup = new JButton("Remove group");
-    private JButton bAddStudent = new JButton("Add student");
-    private JButton bRemoveStudent = new JButton("Remove student");
+    private JButton bAddGroup = new JButton("+");
+    private JButton bRemoveGroup = new JButton("-");
+    private JButton bAddStudent = new JButton("+");
+    private JButton bRemoveStudent = new JButton("-");
     private JButton bExit = new JButton("Exit");
+    private JButton bEvaluate = new JButton("Evaluate");
+    private JButton bEvaluateAll = new JButton("Evaluate all");
+    private JLabel lStudent = new JLabel("Students:");
+    private JLabel lGroup = new JLabel("Group:");
     private JTextArea tStudentScreen = new JTextArea();
-    private JScrollPane spGroup;
+    private JScrollPane spGroup =new JScrollPane();;
     
-    private Uni(){
+    public Uni(){
         setSize(FRAME_WIDTH,FRAME_HEIGHT);
+        setResizable(false);
         setTitle("University");
         setLayout(null);
         
         //Buttons
-        createButton(bAddGroup, "Add group", x,++y*50);
-        createButton(bRemoveGroup, "Remove group", x,++y*50);
-        createButton(bAddStudent, "Add student", x,++y*50);
+        createAddRemoveButton(bAddGroup, 90,15, "Add Group");
+        createAddRemoveButton(bRemoveGroup, 140,15, "Remove Group");
+        createAddRemoveButton(bAddStudent, 320,15, "Add Student");
+        createAddRemoveButton(bRemoveStudent,370,15, "Remove Student");
         
-        createButton(bRemoveStudent, "Remove student",x,++y*50);
-        createButton(bExit, "Exit",x,++y*50);
+        createButton(bExit,450,500);
+        createButton(bEvaluate,325,400);
+        createButton(bEvaluateAll,450,400);
         
        //TextField
-       createTextArea(tStudentScreen, 200,20,FRAME_WIDTH-60,FRAME_HEIGHT-200);
+       createTextArea(tStudentScreen, 200,40,350,350);
        
+       //Label
+       createLabel(lStudent,200,10,100,25);
+       createLabel(lGroup,10,10,100,25);
        
-       spGroup=new JScrollPane();
-       spGroup.setBounds(20,20,120,200);
-       add(spGroup);
+       //ScrollPane
+       createScrollPane(spGroup,20,40,120,200);
+
 
     }
     
-    private static void createAndShowGUI(){
+    public static void createAndShowGUI(){
         Uni uni = new Uni();
         uni.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         uni.setVisible(true);
@@ -63,10 +77,16 @@ public class Uni extends JFrame implements ActionListener{
     
     public static void main(String[] args){
         createAndShowGUI();
-    }
-
-    private void createButton(JButton name, String description, int x, int y){
-       name.setBounds(x,y,130,20);
+    } 
+    private void createAddRemoveButton(JButton name, int x, int y, String ToolTip){
+       name.setBounds(x,y,41,20);
+       name.setToolTipText(ToolTip);
+       add(name);
+       name.addActionListener(this);
+       }
+    
+    private void createButton(JButton name, int x, int y){
+       name.setBounds(x,y,100,20);
        add(name);
        name.addActionListener(this);
        }
@@ -76,6 +96,19 @@ public class Uni extends JFrame implements ActionListener{
        name.setEditable(false);
        name.setText("");
        add(name);
+    }
+    
+    private void createLabel(JLabel name, int xp, int yp, int xk,int yk){
+       name.setBounds(xp,yp,xk,yk);
+       name.setForeground(new Color(0,180,180));
+       name.setFont(new Font("SansSerif",Font.BOLD,20));
+       add(name);
+    }
+    
+    private void createScrollPane(JScrollPane name, int xp, int yp, int xk,int yk){
+       name.setBounds(xp,yp,xk,yk);
+       add(spGroup);
+
     }
     
     /// to odpowiada za reakcje guzikow
@@ -91,10 +124,10 @@ public class Uni extends JFrame implements ActionListener{
             String text = tStudentScreen.getText();
             tStudentScreen.setText(text+ student.getName() + "\n");
         }
-        if(source == bRemoveStudent){
+        else if(source == bRemoveStudent){
             
         }
-        if(source == bExit){
+        else if(source == bExit){
           dispose();
         }
     }
