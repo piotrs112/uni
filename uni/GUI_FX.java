ade430f5f62741a705ce73e7053f777a8fb883b9
piotrs112 <piotrsocha01@gmail.com>
2018-04-06 22:23:08 +0200

    gui działa niezle xd

--- uni/GUI_FX.java
+++ uni/GUI_FX.java
@@ -1,6 +1,8 @@
 package uni;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -16,10 +18,46 @@ public class GUI_FX extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("GUI_FX.fxml"));
 
-        Scene scene = new Scene(root, 600, 400);
+        Scene scene = new Scene(root);
 
-        stage.setTitle("FXML Welcome");
+        stage.setTitle("Uni");
         stage.setScene(scene);
         stage.show();
     }
+
+    @FXML
+    private void grade(ActionEvent e){
+        System.out.println("Grade");
+    }
+
+    @FXML
+    private void gradeAll(ActionEvent e){
+        System.out.println("Grade all");
+    }
+
+    @FXML
+    private void gradeAllInGroup(ActionEvent e){
+        System.out.println("Grade group");
+    }
+
+    @FXML
+    private void addStudent(ActionEvent e){
+        System.out.println("Add student");
+    }
+
+    @FXML
+    private void removeStudent(ActionEvent e){
+        System.out.println("rm student");
+    }
+
+    @FXML
+    private void addGroup(ActionEvent e){
+        System.out.println("add gr");
+    }
+
+    @FXML
+    private void removeGroup(ActionEvent e){
+        System.out.println("rm gr");
+    }
+
 }
