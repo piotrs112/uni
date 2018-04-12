088b1cd9234f0a2b58fe0e835821821a02586970
Sadaffi <37930369+Sadaffi@users.noreply.github.com>
2018-04-12 17:01:02 +0200

    Add files via upload

---
+++ uni/src/uni/IStudent.java
@@ -0,0 +1,9 @@
+package uni;
+
+public interface IStudent extends Comparable<Student> {
+    String getName();
+    void setName(String name);
+    float getGrade();
+    void setGrade(float grade);
+    void evaluate();
+}
